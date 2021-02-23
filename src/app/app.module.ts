469bf9037cc99513447fc1bdb5e730a9015e3cdd
Derek <19533239+derekwolters@users.noreply.github.com>
2021-02-23 17:15:32 -0500

    add components + add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormComponent } from './form/form.component';
-import { ResultComponent } from './src/app/result/result.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [	
